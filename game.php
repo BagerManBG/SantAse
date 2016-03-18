4b792f780ee3808da09303fd4e2652896d483a74
tonyparaskowa <toni_9820@abv.bg>
2016-03-18 22:07:37 +0200

    к

---
+++ game.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SantAse</title>
+
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
+	<script type="text/javascript" src="JS/main.js"></script>
+
+	<link rel="shortcut icon" type="image/png" href="images/favicon/favicon.ico">
+
+	<link rel="stylesheet" type="text/css" href="CSS/reset.css">
+	<link rel="stylesheet" type="text/css" href="CSS/main.css">
+	<link rel="stylesheet" type="text/css" href="CSS/home.css">
+</head>
+<body>
+	<nav>
+		<div class="middleNav">
+			<a href="index.php" class="disable-select">Home</a>
+			<a href="rules.php" class="disable-select">Rules</a>
+			<a href="#" class="disable-select">Contact</a>
+			<a href="#" class="activePage disable-select">Game</a>
+
+			
+		</div>
+	</nav>
+
+</body>
+</html>
