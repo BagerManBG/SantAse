fd54e0d6743fb057988fcf156c065e60fbfb59a9
BagerManBG <toshko_1998@abv.bg>
2016-03-22 22:44:56 +0200

    register valid

--- includes/form.php
+++ includes/form.php
@@ -6,25 +6,25 @@
 		<label>
 			<!--<p>Username:</p>-->
 			<input type="text" name="username" placeholder="Username" id="username">
-
+			<p class="username">*Required</p>
 		</label>
 
 		<label>
 			<!--<p>Email:</p>-->
 			<input type="text" name="email" placeholder="Email" id="email">
-			
+			<p class="email">*Required</p>
 		</label>
 
 		<label>
 			<!--<p>Password:</p>-->
 			<input type="password" name="password" placeholder="Password">
-			
+			<p class="password">*Required</p>
 		</label>
 
 		<label>
 			<!--<p>Confirm:</p>-->
 			<input type="password" name="password_confirm" placeholder="Confirm Password">
-			
+			<p class="password_confirm">*Required</p>
 		</label>
 
 		<input type="submit" value="Sign Up" class="reg disabled" disabled="disabled">
